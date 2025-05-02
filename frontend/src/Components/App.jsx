568d037fbd2654192549acec4fb655e4c8de80c0
samspillers <spillers.sam@gmail.com>
2025-05-01 23:43:14 -0700

    init google login and more debugging

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -1,14 +1,30 @@
 import React from "react";
-// import Main from './Main';
-// import { GoogleOAuthProvider } from '@react-oauth/google';
-// import { GOOGLE_CLIENT_ID } from '../Util/globalConsts'
 
 export default class App extends React.Component {
-    render() {
+    componentDidMount() {
+        console.log(process.env.NODE_ENV);
         console.log(process.env.GOOGLE_CLIENT_ID);
-        return <p>test</p>
-        // return <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-        //     <Main/>
-        // </GoogleOAuthProvider>
+
+        // const client = gapi.auth2.init({})
+
+        // const client = google.accounts.oauth2.initTokenClient({
+        //     client_id: 'clientId',
+        //     scope: 'openid',
+        //     callback: (response) => {
+        //         // Handle the response here
+        //         console.log(response);
+        //     },
+        //     error_callback: (error) => {
+        //         console.log(error);
+        //     }
+        // });
+        // client.requestAccessToken();
+    }
+
+    render() {
+        return <div>
+            <p>test</p>
+            <small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small>
+        </div>
     }
 }
