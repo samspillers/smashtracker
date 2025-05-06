d85417f6eda58897b2981dd29ab18cddbe90d48d
samspillers <spillers.sam@gmail.com>
2025-05-05 19:00:09 -0700

    remove last debug logs

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default class App extends React.Component {
     componentDidMount() {
         console.log(process.env.NODE_ENV);
-        console.log(process.env.AMPLIFY_DIFF_DEPLOY);
-        console.log(process.env.AMPLIFY_MONOREPO_APP_ROOT);
+        // console.log(process.env.AMPLIFY_DIFF_DEPLOY);
+        // console.log(process.env.AMPLIFY_MONOREPO_APP_ROOT);
         console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID);
 
         // const client = gapi.auth2.init({})
